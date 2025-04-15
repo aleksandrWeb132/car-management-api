e315e4d15a318f2a51a77bbca1f070bc5b1a3aaf
Александр Тимохин <a.timohin.prog@solo-it.ru>
2025-04-15 11:43:15 +0400

    Создал контроллер для работы с машинами.

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CarController extends Controller {
+    public function available() {
+        return response()->json(["test"]);
+    }
+}
