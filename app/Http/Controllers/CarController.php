199888dfb9584d23d87d3cdc56f4fa74209eb8dc
Александр Тимохин <a.timohin.prog@solo-it.ru>
2025-04-15 14:26:20 +0400

    Реализовал получение списка машин с конфигурациями опциями и ценой.

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -2,10 +2,50 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Car;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class CarController extends Controller {
     public function available() {
-        return response()->json(["test"]);
+        $currentDate = Carbon::now()->toDateString();
+
+        $cacheKey = 'car_configurations:' . $currentDate;
+
+        $formattedResult = Cache::remember($cacheKey, 600, function () use ($currentDate) {
+            $cars = Car::with(['configurations' => function ($query) use ($currentDate) {
+                $query->with(['options'])
+                    ->whereHas('prices', function ($query) use ($currentDate) {
+                        $query->where('start_date', '<=', $currentDate)
+                            ->where(function ($q) use ($currentDate) {
+                                $q->whereNull('end_date')
+                                    ->orWhere('end_date', '>=', $currentDate);
+                            });
+                    })
+                ;
+            }])->get();
+
+            return $this->formatCarData($cars);
+        });
+
+        return response()->json($formattedResult);
+    }
+
+    private function formatCarData($cars)
+    {
+        return $cars->map(function ($car) {
+            return [
+                'id' => $car->id,
+                'name' => $car->name,
+                'configurations' => $car->configurations->map(function ($configuration) {
+                    return [
+                        'id' => $configuration->id,
+                        'name' => $configuration->name,
+                        'options' => $configuration->options->pluck('name'),
+                        'current_price' => $configuration->prices->first()->price ?? null,
+                    ];
+                }),
+            ];
+        });
     }
 }
