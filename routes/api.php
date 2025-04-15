1201a08341987a6efe0750b9cecda1fbbf4cd64a
Александр Тимохин <a.timohin.prog@solo-it.ru>
2025-04-15 11:51:37 +0400

    Добавил маршрут к /cars/available

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/cars/available', [CarController::class, 'available']);
